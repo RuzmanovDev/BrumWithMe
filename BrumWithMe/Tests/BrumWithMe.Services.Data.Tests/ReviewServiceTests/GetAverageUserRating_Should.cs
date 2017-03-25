759134e0d276317fac7d90534d991d8434a26a35
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-03-25 21:30:49 +0200

    finished testing services

---
+++ BrumWithMe/Tests/BrumWithMe.Services.Data.Tests/ReviewServiceTests/GetAverageUserRating_Should.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using BrumWithMe.Data.Contracts;
+using BrumWithMe.Data.Models.Entities;
+using BrumWithMe.Services.Data.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace BrumWithMe.Services.Data.Tests.ReviewServiceTests
+{
+    [TestFixture]
+    public class GetAverageUserRating_Should
+    {
+        [Test]
+        public void CallGetAllMethodOfReviewRepoOnce_AndReturnAverageRating()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            var service = new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object);
+            var userId = "userId";
+
+            var data = new List<double>()
+            {
+               5, 4, 5, 5, 3
+            };
+
+            mockedReviewRepo.Setup(x => x.GetAll(It.IsAny<Expression<Func<Review, bool>>>(),
+                   It.IsAny<Expression<Func<Review, double>>>()))
+                   .Returns(data);
+
+            var expectedRating = data.Average();
+
+            // Act
+            var result = service.GetAverageUserRating(userId);
+
+            // Assert
+            mockedReviewRepo.Verify(x => x.GetAll(It.IsAny<Expression<Func<Review, bool>>>(),
+                It.IsAny<Expression<Func<Review, double>>>()),
+                Times.Once);
+
+            Assert.AreEqual(expectedRating, result);
+        }
+    }
+}
---
+++ BrumWithMe/Tests/BrumWithMe.Services.Data.Tests/ReviewServiceTests/GetCommentsFor_Should.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BrumWithMe.Data.Contracts;
+using BrumWithMe.Data.Models.Entities;
+using BrumWithMe.Services.Data.Services;
+using Moq;
+using NUnit.Framework;
+using BrumWithMe.Data.Models.CompositeModels.Review;
+using System.Linq.Expressions;
+
+namespace BrumWithMe.Services.Data.Tests.ReviewServiceTests
+{
+    [TestFixture]
+    public class GetCommentsFor_Should
+    {
+        [Test]
+        public void ThrowArgumenNulLException_WhenUserIdIsNull()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            var service = new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => service.GetCommentsFor(null, It.IsAny<int>()));
+        }
+
+        [Test]
+        public void ThrowArgumentException_WhenUserIdIsEmpty()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            var service = new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => service.GetCommentsFor(string.Empty, It.IsAny<int>()));
+        }
+
+        [Test]
+        public void ShouldCallGetAllMappedOnce_WithCorrectParams()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            var service = new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object);
+            var page = 1;
+            var userId = "userid";
+
+            mockedReviewRepo.Setup(x => x.GetAllMapped<DateTime, CommentInfo>(It.IsAny<Expression<Func<Review, bool>>>(),
+              It.IsAny<Expression<Func<Review, DateTime>>>(), page, 5));
+
+            // Act
+            service.GetCommentsFor(userId, page);
+
+            // Assert
+            mockedReviewRepo.Verify(x => x.GetAllMapped<DateTime, CommentInfo>(It.IsAny<Expression<Func<Review, bool>>>(),
+               It.IsAny<Expression<Func<Review, DateTime>>>(), page, 5)
+            , Times.Once);
+        }
+    }
+}
