8e57fe4bd135df79169d8388201353082c96cbc3
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-03-25 22:40:29 +0200

    HomeController Tests

---
+++ BrumWithMe/Tests/BrumWithMe.Mvc.Tests/Controllers/HomeControllerTests/Index_Should.cs
@@ -0,0 +1,29 @@
+﻿using BrumWithMe.MVC.Controllers;
+using BrumWithMe.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace BrumWithMe.Mvc.Tests.Controllers.HomeControllerTests
+{
+    [TestFixture]
+    class Index_Should
+    {
+        [Test]
+        public void Render_DefaultView()
+        {
+            // Arrange
+            var mockedAccountManagementService = new Mock<IAccountManagementService>();
+            var controller = new HomeController(mockedAccountManagementService.Object);
+
+            // Act and Assert
+            controller.WithCallTo(c => c.Index(0))
+               .ShouldRenderDefaultView();
+        }
+    }
+}
---
+++ BrumWithMe/Tests/BrumWithMe.Mvc.Tests/Controllers/HomeControllerTests/LoginPartial_Should.cs
@@ -0,0 +1,46 @@
+﻿using BrumWithMe.MVC.Controllers;
+using BrumWithMe.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace BrumWithMe.Mvc.Tests.Controllers.HomeControllerTests
+{
+    [TestFixture]
+    public class LoginPartial_Should
+    {
+        [Test]
+        public void ReturnLoginPartiaL_WhenThereIsNoLoggedUser()
+        {
+            // Arrange
+            var mockedAccountManagementService = new Mock<IAccountManagementService>();
+            var controller = new HomeController(mockedAccountManagementService.Object);
+
+            controller.GetLoggedUserId = () => null;
+
+            // Act and Assert
+            controller.WithCallTo(c => c.LoginPartial())
+              .ShouldRenderPartialView("_LoginPartial");
+        }
+
+        [Test]
+        public void ReturnLogginPartial_WithLoggedUserAvatarInIt_WhenThereIsLoggedUser()
+        {
+            // Arrange
+            var mockedAccountManagementService = new Mock<IAccountManagementService>();
+            var controller = new HomeController(mockedAccountManagementService.Object);
+
+            var loggedUserId = "userId";
+            var loggedUserAvatarUrl = "avatar";
+            controller.GetLoggedUserId = () => loggedUserId;
+
+            mockedAccountManagementService.Setup(x => x.GetUserAvatarUrl(loggedUserId))
+                .Returns(loggedUserAvatarUrl);
+
+            // Act and Assert
+            controller.WithCallTo(c => c.LoginPartial())
+              .ShouldRenderPartialView("_LoginPartial")
+              .WithModel(loggedUserAvatarUrl);
+        }
+    }
+}
