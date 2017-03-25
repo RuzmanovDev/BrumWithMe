b0ef1e87b4d5b26c03eb3e1f3a2280f30098ca07
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-03-25 23:56:41 +0200

    ReportController tests

--- BrumWithMe/Tests/BrumWithMe.Mvc.Tests/Controllers/ReportControllerTests/Constructor_Should.cs
+++ BrumWithMe/Tests/BrumWithMe.Mvc.Tests/Controllers/ReportControllerTests/Constructor_Should.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BrumWithMe.MVC.Controllers;
+using BrumWithMe.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
 
 namespace BrumWithMe.Mvc.Tests.Controllers.ReportControllerTests
 {
-    class Constructor_Should
+    [TestFixture]
+    public class Constructor_Should
     {
+        [Test]
+        public void ThrowArgumentExcpetion_WithMessaveContainingReportService_WhenReportServiceIsNull()
+        {
+            // Arrange
+            IReportService reportService = null;
+
+            // Act and Assert
+            Assert.That(() => new ReportController(reportService),
+                Throws.ArgumentNullException.With.Message.Contain(nameof(reportService)));
+        }
+
+        [Test]
+        public void DoesNotThrow_WhenAllArgumentsAreValid()
+        {
+            // Arrange
+            var mockedReportService = new Mock<IReportService>();
+
+            // Act and Assert
+            Assert.DoesNotThrow(() => new ReportController(mockedReportService.Object));
+        }
     }
 }
---
+++ BrumWithMe/Tests/BrumWithMe.Mvc.Tests/Controllers/ReportControllerTests/ReportTrip_Should.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using BrumWithMe.MVC.Controllers;
+using BrumWithMe.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace BrumWithMe.Mvc.Tests.Controllers.ReportControllerTests
+{
+    [TestFixture]
+    public class ReportTrip_Should
+    {
+        [Test]
+        public void Have_HttpPost_Attribute()
+        {
+            // Arrange
+            var method = typeof(ReportController).GetMethod(nameof(ReportController.ReportTrip));
+
+            // Act
+            var hasAttr = method.GetCustomAttributes(typeof(HttpPostAttribute), false).Any();
+
+            // Assert
+            Assert.IsTrue(hasAttr);
+        }
+
+        [Test]
+        public void CallReportService_WithCorrectParams()
+        {
+            // Arrange
+            var mockedReportService = new Mock<IReportService>();
+            var tripId = 1;
+            var controller = new ReportController(mockedReportService.Object);
+
+            // Act
+            controller.ReportTrip(tripId);
+
+            // Assert
+            mockedReportService.Verify(x => x.ReportTrip(tripId), Times.Once);
+        }
+    }
+}
