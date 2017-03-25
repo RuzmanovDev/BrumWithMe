ffbbac040fad37b31e3ca3c7b090cacb62b1be35
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-03-25 22:18:36 +0200

    code coverage please don't go down :(

---
+++ BrumWithMe/Tests/BrumWithMe.Mvc.Tests/Controllers/HomeControllerTests/Constructor_Should.cs
@@ -0,0 +1,32 @@
+﻿using BrumWithMe.MVC.Controllers;
+using BrumWithMe.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace BrumWithMe.Mvc.Tests.Controllers.HomeControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNulLException_WhenAccountManagementService_IsNull()
+        {
+            // Arrange
+            IAccountManagementService accountManagementService = null;
+
+            // Act and Assert
+            Assert.That(() => new HomeController(accountManagementService),
+                Throws.ArgumentNullException.With.Message.Contain(nameof(accountManagementService)));
+        }
+
+        [Test]
+        public void DoesNotThrow_WhenAllArugmentsAreValdi()
+        {
+            // Arrange
+            var mockedAccountManagementService = new Mock<IAccountManagementService>();
+
+            // Act and Assert
+            Assert.DoesNotThrow(() => new HomeController(mockedAccountManagementService.Object));
+        }
+    }
+}
