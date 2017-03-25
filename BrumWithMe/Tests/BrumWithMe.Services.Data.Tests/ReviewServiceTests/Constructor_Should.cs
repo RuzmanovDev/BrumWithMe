dae1115cfcda2dcc7ef5fb3ebae27c04db15e2ff
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-03-25 21:09:53 +0200

    more tessts

---
+++ BrumWithMe/Tests/BrumWithMe.Services.Data.Tests/ReviewServiceTests/Constructor_Should.cs
@@ -0,0 +1,35 @@
+﻿using BrumWithMe.Data.Contracts;
+using BrumWithMe.Data.Models.Entities;
+using BrumWithMe.Services.Data.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace BrumWithMe.Services.Data.Tests.ReviewServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WithMessageContainingReviews_WhenReviewsRepoIsNull()
+        {
+            // Arrange
+            IProjectableRepositoryEf<Review> reviews = null;
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            // Act and Assert
+            Assert.That(() => new ReviewService(reviews, () => mockedUnitOfWork.Object),
+                Throws.ArgumentNullException.With.Message.Contain(nameof(reviews)));
+        }
+
+        [Test]
+        public void ReturnAnInstance_AndNotThrow_WhenAllArgumentsAreValid()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            // Act and Assert
+            Assert.DoesNotThrow(() => new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object));
+        }
+    }
+}
---
+++ BrumWithMe/Tests/BrumWithMe.Services.Data.Tests/ReviewServiceTests/CreateReview_Should.cs
@@ -0,0 +1,52 @@
+﻿using BrumWithMe.Data.Contracts;
+using BrumWithMe.Data.Models.Entities;
+using BrumWithMe.Services.Data.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrumWithMe.Services.Data.Tests.ReviewServiceTests
+{
+    [TestFixture]
+    public class CreateReview_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WithMessageContainingReview_WhenReviewIsNull()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            var service = new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object);
+
+            Review review = null;
+
+            // Act and Assert
+            Assert.That(() => service.CreateReview(review),
+                Throws.ArgumentNullException.With.Message.Contain(nameof(review)));
+        }
+
+        [Test]
+        public void CallAddMethodFromTheReviewRepo_OnceWithPassedAsparameterReview()
+        {
+            // Arrange
+            var mockedReviewRepo = new Mock<IProjectableRepositoryEf<Review>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+            var service = new ReviewService(mockedReviewRepo.Object, () => mockedUnitOfWork.Object);
+
+            Review review = new Review();
+
+            // Act
+            service.CreateReview(review);
+
+            // Assert
+            mockedReviewRepo.Verify(x => x.Add(review), Times.Once);
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+    }
+}
