17cfbb2ded8d54c2b939d9f8829a145d97e5e663
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-03-25 14:24:39 +0200

    Finished tess for tripService

---
+++ BrumWithMe/Tests/BrumWithMe.Services.Data.Tests/TripServiceTests/DeleteTrip_Should.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using BrumWithMe.Data.Contracts;
+using BrumWithMe.Data.Models.Entities;
+using BrumWithMe.Services.Data.Contracts;
+using BrumWithMe.Services.Data.Services;
+using BrumWithMe.Services.Providers.Mapping.Contracts;
+using BrumWithMe.Services.Providers.TimeProviders;
+using Moq;
+using NUnit.Framework;
+
+namespace BrumWithMe.Services.Data.Tests.TripServiceTests
+{
+    [TestFixture]
+    public class DeleteTrip_Should
+    {
+        [Test]
+        public void MarkTripAsDeleted()
+        {
+            // Arrange
+            var mockedTripRepo = new Mock<IProjectableRepositoryEf<Trip>>();
+            var mockedUserTripRepo = new Mock<IProjectableRepositoryEf<UsersTrips>>();
+            var mockedCityService = new Mock<ICityService>();
+            var mockedTagService = new Mock<ITagService>();
+            var mockedDateTimpeProvider = new Mock<IDateTimeProvider>();
+            var mockedMappingProvider = new Mock<IMappingProvider>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+
+            var tripService = new TripService(
+                  () => mockedUnitOfWork.Object,
+                  mockedUserTripRepo.Object,
+                  mockedCityService.Object,
+                  mockedMappingProvider.Object,
+                  mockedTagService.Object,
+                  mockedTripRepo.Object,
+                  mockedDateTimpeProvider.Object);
+
+            var trip = new Trip() { Id = 1 };
+            mockedTripRepo.Setup(x => x.GetFirst(It.IsAny<Expression<Func<Trip, bool>>>()))
+                .Returns(trip);
+
+            // Act
+            tripService.DeleteTrip(trip.Id);
+
+            // Assert;
+            Assert.IsTrue(trip.IsDeleted);
+        }
+
+    }
+}
---
+++ BrumWithMe/Tests/BrumWithMe.Services.Data.Tests/TripServiceTests/RecoverTrip_Should.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using BrumWithMe.Data.Contracts;
+using BrumWithMe.Data.Models.Entities;
+using BrumWithMe.Services.Data.Contracts;
+using BrumWithMe.Services.Data.Services;
+using BrumWithMe.Services.Providers.Mapping.Contracts;
+using BrumWithMe.Services.Providers.TimeProviders;
+using Moq;
+using NUnit.Framework;
+
+namespace BrumWithMe.Services.Data.Tests.TripServiceTests
+{
+    [TestFixture]
+   public class RecoverTrip_Should
+    {
+        [Test]
+        public void MarkTripAsNotDeleted()
+        {
+
+            // Arrange
+            var mockedTripRepo = new Mock<IProjectableRepositoryEf<Trip>>();
+            var mockedUserTripRepo = new Mock<IProjectableRepositoryEf<UsersTrips>>();
+            var mockedCityService = new Mock<ICityService>();
+            var mockedTagService = new Mock<ITagService>();
+            var mockedDateTimpeProvider = new Mock<IDateTimeProvider>();
+            var mockedMappingProvider = new Mock<IMappingProvider>();
+            var mockedUnitOfWork = new Mock<IUnitOfWorkEF>();
+
+
+            var tripService = new TripService(
+                  () => mockedUnitOfWork.Object,
+                  mockedUserTripRepo.Object,
+                  mockedCityService.Object,
+                  mockedMappingProvider.Object,
+                  mockedTagService.Object,
+                  mockedTripRepo.Object,
+                  mockedDateTimpeProvider.Object);
+
+            var trip = new Trip() { Id = 1, IsDeleted = true };
+            mockedTripRepo.Setup(x => x.GetFirst(It.IsAny<Expression<Func<Trip, bool>>>()))
+                .Returns(trip);
+
+            // Act
+            tripService.RecoverTrip(trip.Id);
+
+            // Assert;
+            Assert.IsFalse(trip.IsDeleted);
+        }
+    }
+}
